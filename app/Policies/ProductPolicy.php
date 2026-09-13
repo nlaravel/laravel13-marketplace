4941e4b7b46b3e96eeec2a9bcd52d0b3e1a1559f
nlaravel <n.laravel.developer@gmail.com>
2026-09-13 22:05:54 +0300

    solve product policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -12,9 +12,7 @@ class ProductPolicy
 {
     public function view(User $user, Product $product): bool
     {
-        return $product->store
-            ->seller
-            ->user_id === $user->id;
+        return $this->ownsProduct($user, $product);
     }
 
     public function create(User $user): bool
@@ -32,15 +30,16 @@ class ProductPolicy
 
     public function update(User $user, Product $product): bool
     {
-        return $product->store
-            ->seller
-            ->user_id === $user->id;
+        return $this->ownsProduct($user, $product);
     }
 
     public function delete(User $user, Product $product): bool
     {
-        return $product->store
-            ->seller
-            ->user_id === $user->id;
+        return $this->ownsProduct($user, $product);
+    }
+
+    private function ownsProduct(User $user, Product $product): bool
+    {
+        return $product->store?->seller?->user_id === $user->id;
     }
 }
